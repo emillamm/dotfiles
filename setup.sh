a6386192a865416fb124398312b1cf6a26fa51f7
Emil Nielsen <enielsen@grubhub.com>
2018-09-07 17:50:52 -0400

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
-dir=$(dirname $0)
-for filename in $dir/dotfiles/*; do
+pdir=$(pwd)
+cdir=$(dirname $0 | sed -e 's/^\.//g') # remove leading .
+dir=$(echo $pdir/$cdir/dotfiles | sed -e 's/\/\/*/\//g') # remove double slashes
+for filename in $dir/*; do
     file=$(basename $filename)
     if [ ! -e ~/.$file ]
         then
 	echo "copying $file"
-        ln -s $dir/dotfiles/$file ~/.$file
+        ln -s $dir/$file ~/.$file
     else
         echo "$file already copied"
     fi
 done
+
